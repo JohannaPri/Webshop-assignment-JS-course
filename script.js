a86f6346d499c4413d59c1344966da04473cd3a4
Johanna Prinz <Johannaa.L@hotmail.com>
2023-11-15 19:55:31 +0100

    changes in files

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 /**
- *
+ * Skapa array med object i
+ * ska funktion för att generera sections
+ * Skapa loop i funktioner för att loopa igenom objekten i arrayen
+ * Dynamiskt lägga in värden från objekten i varje container
  */
+
+
 
